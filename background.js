72b4a85ec67491207d0c95930c71819cc01dc9fc
MoustikBe <maxenceic2@gmail.com>
2025-12-16 11:46:16 +0100

    pushing holy_graph change

--- background.js
+++ background.js
@@ -1,1 +1,28 @@
 /* Ici on va gerer tout les elements du background */
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: [42],
+        addRules: [
+            {
+                id: 42,
+                priority: 1,
+                action: {
+                    type: "redirect",
+                    redirect: {
+                        transform: {
+                            queryTransform: {
+                                addOrReplaceParams: [
+                                    { key: "campus_id", value: "12" }
+                                ]
+                            }
+                        }
+                    }
+                },
+                condition: {
+                    urlFilter: "project_data.json",
+                    resourceTypes: ["xmlhttprequest"]
+                }
+            }
+        ]
+    });
+});
